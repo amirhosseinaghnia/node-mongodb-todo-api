d63d7082f692840c41b7bb43d2d8f1c8968de772
amirhossein <amirhossin1908@gmail.com>
2018-04-28 23:12:59 +0430

    Add GET/todos/:id tests and fix few bugs in package.json

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const chai = require('chai');
 const should = chai.should();
 const request = require('supertest');
+const{ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const{Todo} = require('./../models/todo');
 
 var todos = [{
+    _id: new ObjectID(),
     text: 'firs todo test'
 }, {
+    _id: new ObjectID(),
     text: 'second todo test'
 }];
 
@@ -78,5 +81,31 @@ describe('GET /todos', () => {
 
     });
 
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                res.body.todo.text.should.equal(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
 });
 
