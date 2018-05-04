65d7c9ed0f409cdb9c365536f16caf893911baaf
amirhossein <amirhossin1908@gmail.com>
2018-05-05 01:01:32 +0430

    Add playground/mongoose-remove file and Add Delete /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+Todo.findByIdAndRemove('5aec87e920733b2d9cb460d4').then((todo) => {
+    console.log(todo);
+})
