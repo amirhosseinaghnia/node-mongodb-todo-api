51b437468eeb281f9f4b76a7e60726eabc19f420
amirhossein <amirhossin1908@gmail.com>
2018-05-28 00:51:22 +0430

    Add some test cases in server.test.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -46,7 +46,7 @@ describe('POST /todos', () =>{
                     todos[0].text.should.equal(text);
                     done();
                 }).catch((e) => done(e));
-            })
+            });
     });
 
     it('should not creat todo with invalid body data', (done) => {
@@ -108,4 +108,46 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/id:', () => {
+    
+    it('should remove a todo', (done) => {
+
+        var hexId = todos[0]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                res.body.todo._id.should.equal(hexId);
+            })
+            .end((err, res) => {
+                
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(todos[0]._id).then((todo) => {
+                    should.not.exist(todo);
+                    done();
+                }).catch((err) => done(err));
+
+            });
+    });
+
+    it('should rturn 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should rturn 404 if object id is invalid', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+
+})
 
